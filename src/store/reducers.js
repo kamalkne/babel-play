2418d7e29a4cc4335a3020268e497145ba0e6ba5
Kamal Kokne <kamalkokne4@gmail.com>
2018-02-16 12:36:55 +0530

    Combined all reducers into one using redux.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,6 @@
 import C from '../constants';
 import App from '../appconstants';
+import { combineReducers } from 'redux';
 
 export const name = (state = '', action) => {
     if (action.type === C.UPDATE_NAME) {
@@ -9,10 +10,14 @@ export const name = (state = '', action) => {
     }
 }
 
-
 export const status = (state = App.STATUS[1], action) => {
     if (action.type === C.UPDATE_STATUS && state !== action.payload) {
         return action.payload;
     }
     return state;
 }
+
+export default combineReducers({
+    name,
+    status
+});
