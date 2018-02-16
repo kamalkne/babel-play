e9c8f745657b51186ba2801df435e265512f494c
Kamal Kokne <kamalkokne4@gmail.com>
2018-02-16 11:53:31 +0530

    Added new reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,4 +1,5 @@
 import C from '../constants';
+import App from '../appconstants';
 
 export const name = (state = '', action) => {
     if (action.type === C.UPDATE_NAME) {
@@ -6,4 +7,12 @@ export const name = (state = '', action) => {
     } else {
         return state;
     }
+}
+
+
+export const status = (state = App.STATUS[1], action) => {
+    if (action.type === C.UPDATE_STATUS && state !== action.payload) {
+        return action.payload;
+    }
+    return state;
 }
